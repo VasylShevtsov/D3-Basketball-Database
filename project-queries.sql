c5886edd23ed1a12fecf4ccaa896e50c60fb8452
AidanVB <vonbucha@whitman.edu>
2024-05-01 23:31:51 -0700

    got rid of top five scorers, saved it in a doc tho for later

--- project-queries.sql
+++ project-queries.sql
@@ -177,25 +177,6 @@ BEGIN
     WHERE PlayerID = p_PlayerID;
 END;
 
--- Get Top Scorers From a Specific Game Procedure
-DROP PROCEDURE IF EXISTS GetTopScorersInGame;
-CREATE PROCEDURE GetTopScorersInGame(
-    IN p_GameID SMALLINT,
-    IN p_Limit INT DEFAULT 5
-)
-BEGIN
-    SELECT 
-        p.PlayerID,
-        p.FirstName,
-        p.LastName,
-        ps.Points
-    FROM PlayerGameStatistic ps
-    JOIN Player p ON ps.PlayerID = p.PlayerID
-    WHERE ps.GameID = p_GameID
-    ORDER BY ps.Points DESC
-    LIMIT p_Limit;
-END;
-
 
 -- Get All Games Played By a Team Procedure
 DROP PROCEDURE IF EXISTS GetTeamGameHistory;
