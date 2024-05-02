510c4bf6e5f12f8a18969cabd80e246ea2fcc27d
Vasyl Shevtsov <101153960+VasylShevtsov@users.noreply.github.com>
2024-05-01 23:30:09 -0700

    player vs player proedure

--- more-procedures.sql
+++ more-procedures.sql
@@ -21,3 +21,27 @@ BEGIN
     WHERE TeamID = p_TeamID AND YEAR(Game.Date) = p_SeasonYear;
 END;
 
+CREATE PROCEDURE ComparePlayers(IN p_PlayerID1 SMALLINT, IN p_PlayerID2 SMALLINT)
+BEGIN
+    SELECT 
+        PlayerID,
+        AVG(FieldGoalsMade) AS AvgFieldGoalsMade,
+        AVG(FieldGoalsAttempted) AS AvgFieldGoalsAttempted,
+        AVG(ThreePointersMade) AS AvgThreePointersMade,
+        AVG(ThreePointersAttempted) AS AvgThreePointersAttempted,
+        AVG(FreeThrowsMade) AS AvgFreeThrowsMade,
+        AVG(FreeThrowsAttempted) AS AvgFreeThrowsAttempted,
+        AVG(PersonalFouls) AS AvgPersonalFouls,
+        AVG(Rebounds) AS AvgRebounds,
+        AVG(OffensiveRebounds) AS AvgOffensiveRebounds,
+        AVG(DefensiveRebounds) AS AvgDefensiveRebounds,
+        AVG(Assists) AS AvgAssists,
+        AVG(Steals) AS AvgSteals,
+        AVG(Blocks) AS AvgBlocks,
+        AVG(Turnovers) AS AvgTurnovers,
+        AVG(Points) AS AvgPoints,
+        AVG(MinutesPlayed) AS AvgMinutesPlayed
+    FROM PlayerGameStatistic
+    WHERE PlayerID IN (p_PlayerID1, p_PlayerID2)
+    GROUP BY PlayerID;
+END;
